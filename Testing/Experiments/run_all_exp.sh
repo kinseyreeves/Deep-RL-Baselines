5bfb23dbb37a69411a8d3ab3ed51f0f473881e59
kreeves <k.reeves@student.unimelb.edu.au>
2020-04-09 17:43:55 +1000

    exp configs

--- Testing/Experiments/run_all_exp.sh
+++ Testing/Experiments/run_all_exp.sh
@@ -1,5 +1,7 @@
+#python maze_experiments.py --steps 1000000 --1reward --name 3goals_capture_reward_200max --num_goals 3
+python maze_experiments.py --steps 1000000 --name 3goals_neg01_reward_on_goal_200max --num_goals 3
 # python reacher_experiments.py
-python maze_experiments.py 500000
+
 #python chaser_experiments.py
 #python evader_experiments.py
 #python chaser_vs_evader_experiments.py
