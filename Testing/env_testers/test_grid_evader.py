380f7c3609086306fbace9ea47201e71ab473349
kinseyreeves <kinseyr1313@gmail.com>
2020-05-14 16:39:10 +1000

    testing code reformat

--- Testing/env_testers/test_grid_evader.py
+++ Testing/env_testers/test_grid_evader.py
@@ -1,17 +1,15 @@
-import gym
-import gym_scalable
-from gym_scalable.envs.grid.maps import map_loader
-import random
-import numpy as np
-import time
 import os
+
+import gym
+from gym_scalable.envs.grid.maps import map_loader
 from pympler.tracker import SummaryTracker
 
 tracker = SummaryTracker()
 
 print(os.getcwd())
-env = gym.make('n-grid_evaders-v0', config= {"mapfile" : map_loader.get_5x5_map(), "randomize_start":True, "randomize_goal":True, "curriculum" : True, "RL_evader":False})
-
+env = gym.make('n-grid_evaders-v0',
+               config={"mapfile": map_loader.get_5x5_map(), "randomize_start": True, "randomize_goal": True,
+                       "curriculum": True, "RL_evader": False})
 
 state = env.reset()
 i = 0
@@ -20,44 +18,42 @@ goal = env.grid.goal
 print(env.observation_space)
 
 while i < 100000:
-    #input()
+    # input()
     i += 1
     env.render()
     s = env.reset()
     print(s)
-    #print(env.controlled_entity.get_randomness())
-
+    # print(env.controlled_entity.get_randomness())
 
     continue
-    #action_ = np.zeros(env.action_space.n)
+    # action_ = np.zeros(env.action_space.n)
 
-    #print(action_)
+    # print(action_)
     action_size = env.action_space.n
-    #action = np.zeros(action_size)
+    # action = np.zeros(action_size)
 
-    #action[env.action_space.sample()] = 1
+    # action[env.action_space.sample()] = 1
 
-    #action_ = env.grid.get_astar_action((env.controlled_entity.x, env.controlled_entity.y), (env.evader.x, env.evader.y))
-    #print(env.controlled_entity.pos)
-    #print(env.evader.pos)
-    #path = env.grid.astar_path(*env.controlled_entity.pos, *env.evader.pos)
-    #print(path)
-    #print(action_)
-    #action_ = np.nonzero(action_)[0][0]
+    # action_ = env.grid.get_astar_action((env.controlled_entity.x, env.controlled_entity.y), (env.evader.x, env.evader.y))
+    # print(env.controlled_entity.pos)
+    # print(env.evader.pos)
+    # path = env.grid.astar_path(*env.controlled_entity.pos, *env.evader.pos)
+    # print(path)
+    # print(action_)
+    # action_ = np.nonzero(action_)[0][0]
     # print(action_)
     # action = 1
     # print(env.grid.start)
     # print(env.grid.goal)
     # print(f"state :  {state}")
 
-
     action_ = env.action_space.sample()
-   # print(action_)
+    # print(action_)
     state, reward, done, _ = env.step(action_)
-    #print(f"{state}  {reward}")
+    # print(f"{state}  {reward}")
 
-    #print(env.normalize_state)
-    if(done):
+    # print(env.normalize_state)
+    if (done):
         env.reset()
 
     input()
--- Testing/env_testers/test_multi_chaser_vs_evader.py
+++ Testing/env_testers/test_multi_chaser_vs_evader.py
@@ -1,14 +1,12 @@
-import gym
-import gym_scalable
-import random
-import numpy as np
-import time
 import os
-from gym_scalable.envs.grid.multi_chaser_evader_env import GridChaserVsEvaderEnv
+import time
+
 from gym_scalable.envs.grid.maps import map_loader
+from gym_scalable.envs.grid.multi_chaser_evader_env import GridChaserVsEvaderEnv
 
 print(os.getcwd())
-env = GridChaserVsEvaderEnv(config= {"mapfile" : map_loader.get_8x8_map(), "RL_evader":False, "full_state" : False, "normalize_state" : True})
+env = GridChaserVsEvaderEnv(
+    config={"mapfile": map_loader.get_8x8_map(), "RL_evader": False, "full_state": False, "normalize_state": True})
 
 state = env.reset()
 i = 0
@@ -21,16 +19,15 @@ while True:
     action1 = env.action_space.sample()
     action2 = env.action_space.sample()
 
-
-    state, reward, done, _ = env.step(actions={"evader":action1, "chaser":action2})
+    state, reward, done, _ = env.step(actions={"evader": action1, "chaser": action2})
 
     print(f"state :  {state}")
     print(f"reward : {reward}")
     print(f"done : {done}")
 
-    #print(env.normalize_state)
-    if(done["__all__"]):
+    # print(env.normalize_state)
+    if (done["__all__"]):
         env.reset()
 
-    #input()
+    # input()
     time.sleep(0.1)
