5ab9cffac9bb153b9977ff0c8c43934dc98f9fa5
kinseyreeves <kinseyr1313@gmail.com>
2020-06-16 17:44:58 +1000

    .

--- Testing/Experiments/bash_runs/dqn.sh
+++ Testing/Experiments/bash_runs/dqn.sh
@@ -1,4 +1,7 @@
 /usr/bin/timeout 1800s python ../maze_experiments.py --rl DQN --name DQN_goals_change --random_goals --random_start --num_goals 1 --map_size 3
-/usr/bin/timeout 1800s python ../maze_experiments.py --rl DQN --name DQN_goals_change --random_goals --random_start --num_goals 1 --map_size 3
-/usr/bin/timeout 1800s python ../maze_experiments.py --rl DQN --name DQN_goals_change --random_goals --random_start --num_goals 1 --map_size 3
-/usr/bin/timeout 1800s python ../maze_experiments.py --rl DQN --name DQN_goals_change --random_goals --random_start --num_goals 1 --map_size 3
+/usr/bin/timeout 1800s python ../maze_experiments.py --rl DQN --name DQN_goals_change --random_goals --random_start --num_goals 1 --map_size 4
+/usr/bin/timeout 1800s python ../maze_experiments.py --rl DQN --name DQN_goals_change --random_goals --random_start --num_goals 1 --map_size 5
+/usr/bin/timeout 1800s python ../maze_experiments.py --rl DQN --name DQN_goals_change --random_goals --random_start --num_goals 1 --map_size 6
+/usr/bin/timeout 1800s python ../maze_experiments.py --rl DQN --name DQN_goals_change --random_goals --random_start --num_goals 1 --map_size 7
+/usr/bin/timeout 1800s python ../maze_experiments.py --rl DQN --name DQN_goals_change --random_goals --random_start --num_goals 1 --map_size 8
+/usr/bin/timeout 1800s python ../maze_experiments.py --rl DQN --name DQN_goals_change --random_goals --random_start --num_goals 1 --map_size 9
